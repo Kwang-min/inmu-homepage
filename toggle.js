85dc2d317e77cdd1168694fcc5b8339e25d440ea
Kwang-min <j2h2677@gmail.com>
2022-01-16 00:58:30 +0900

    모바일에서 네브바 토글 눌렀을 때 하얀바탕으로 바꾸기

--- toggle.js
+++ toggle.js
@@ -1,8 +1,10 @@
 const toggleBtn = document.querySelector(".navbar__toggleBtn");
+const navbar = document.querySelector(".navbar");
 const menu = document.querySelector(".navbar__menu");
 const icons = document.querySelector(".navbar__icons");
 
 toggleBtn.addEventListener("click", () => {
+    navbar.classList.toggle('active')
     menu.classList.toggle('active')
     icons.classList.toggle('active')
 })
